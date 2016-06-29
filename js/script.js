1a922d381a87587e06d7e3a8bbeb991e1849bde1
Lea Ann Bradford <ltrammer@gmail.com>
2016-06-29 14:40:58 -0700

    initial commit
    initial commit. shell for assignment created

---
+++ js/script.js
@@ -0,0 +1,116 @@
+//ASSIGNMENT 4 PRACTICE WITH JAVASCRIPT OOP CONCEPTS 
+syntax for creating a new object
+var variableName = new ObjectType(arguments);
+new object using generic Object type
+var car = new Object();
+
+
+
+
+
+// STEP 1
+/*
+var ANIMAL = ANIMAL || {};
+var cat = new ANIMAL();
+var dog = new ANIMAL();
+create 2 subclasses; cat and dog
+*/
+
+//STEP 2
+/*
+//create cat(literal notation) and dog(constructor notation)
+var cat = {};
+function Dog() {};
+var dog = new Dog();
+
+*/
+
+
+// STEP 3
+/*
+
+*/
+
+// STEP 4
+/*
+
+*/
+
+// STEP 5
+/*
+
+
+*/
+
+// STEP 6
+/*
+var Animal = function(type,breed,color,height,length){
+this.type = ;
+this.breed = ;
+this.color = ;
+this.height = ;
+this.length = ;
+}
+var myAnimal = new Animal
+for (var property in myAnimal) {
+	console.log(property);
+}
+*/
+
+// STEP 7
+/*
+
+*/
+
+// STEP 8
+/*
+
+*/
+
+// STEP 9
+/*
+
+
+*/
+
+// STEP 10 	
+/*
+
+
+*/
+
+// THE RECIPE CARD	
+/*
+Create recipe object
+title
+servings
+ingredients []
+public method function print(title, servings, ingredients){
+	console.log(title + ':\n' + servings + ':\n' + 'Ingredients: \n ' + '-' +ingredients[0] + '\n'
+}
+
+*/
+
+// THE READING LIST
+
+array of objects:
+books{
+	book1 {
+		title:
+		author:
+		read: boolean
+	}
+	book2 {
+		title:
+		author:
+		read:
+	}
+}
+if (read = true) {
+'You already read ' + title + ' by ' + author + '.'
+} else {
+	'You still need to read ' + title + ' by ' + author + '.'
+}
+/*
+
+*/
